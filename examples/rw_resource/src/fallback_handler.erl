a60f110536405eda7009300a72722f48b536434b
Olav Frengstad <olav@fwt.no>
2012-08-24 21:34:12 +0200

    Add a custom 404 catch all to resource example

--- examples/rw_resource/src/fallback_handler.erl
+++ examples/rw_resource/src/fallback_handler.erl
@@ -2,7 +2,7 @@
 
 -behaviour(cowboy_http_handler).
 
--export([methods/1, handle_put/2, handle_req/2, handlers/1 ,handle_patch/2]).
+-export([handlers/1, methods/1, handle_404/2]).
 
 -include_lib("tavern/include/rest_module.hrl").
 
@@ -14,14 +14,6 @@ handlers(_Req) ->
 methods(_Req) ->
 	['HEAD', 'GET', 'POST', 'PUT', 'PATCH'].
 
-handle_get(Req, State) ->
-	{'Ok', Req, State, [{hello, <<"world">>}], Req, State}.
+handle_404(Req, State) ->
+	{'Not Found', Req, State, [{error, [{message, <<"resource not found">>}, {code, 404]}.
 
-handle_req(Req, State) ->
-	{'Ok', Req, State, [{hello, <<"world">>}], Req, State}.
-
-handle_put(Req, #tavern{body = _Body} = State) ->
-	{'Created', Req, State, [{message, <<"world replaces">>}]}.
-
-handle_patch(Req, State) ->
-	{'Ok', Req, State, [{message, <<"new world order">>}]}.
