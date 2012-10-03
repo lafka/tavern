af97bd4a7193e7fd94cae3bc3bea149443d7a8e6
Olav Frengstad <olav@fwt.no>
2012-10-03 22:21:57 +0200

    Add expm package file

---
+++ package.exs
@@ -0,0 +1,7 @@
+Expm.Package.new(
+	name: "tavern",
+	description: "Somewhat RESTfull extension for cowboy without support of hypermedia",
+	version: "0.0.2",
+	keywords:["rest","hypermedia","marshalling","web-service"],
+    maintainers: [[name: "Olav Frengstad", email:"olav@fwt.com"]],
+	repositories: [[github: "lafka/tavern", tag: "0.0.1"]])
