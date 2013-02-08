04fcba3cfa4ad1bae5c745db1403c8884cb236e9
Olav Frengstad <olav@fwt.no>
2013-02-08 12:53:35 +0100

    Don't set 'Allow' header for OPTIONS (set by tavern_http:init)

--- include/rest_module.hrl
+++ include/rest_module.hrl
@@ -16,9 +16,5 @@ terminate(_Reason, Req, State) ->
 
 -spec handle_default_options(_, #tavern{}) -> {Status :: atom(), _, #tavern{}, binary()}.
 handle_default_options(Req, #tavern{allowed_methods = Methods} = State) ->
-	F = fun(X, <<>>) -> atom_to_binary(X, utf8);
-		(X, Acc) -> X2 = atom_to_binary(X, utf8),
-		<<Acc/binary, $,, X2/binary>> end,
-	{ok, Req2} = cowboy_req:set_resp_header('Allow', lists:foldl(F, <<>>, Methods), Req),
 	{'No Content', Req2, State, <<>>}.
 
